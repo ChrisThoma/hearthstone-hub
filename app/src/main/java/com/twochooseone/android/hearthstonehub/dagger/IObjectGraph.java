1875d2f34478158c9c4054394f736c14d76154e3
ChrisThoma <cthoma@vt.edu>
2014-09-13 19:43:47 -0400

    init.

---
+++ app/src/main/java/com/twochooseone/android/hearthstonehub/dagger/IObjectGraph.java
@@ -0,0 +1,10 @@
+package com.twochooseone.android.hearthstonehub.dagger;
+
+import dagger.ObjectGraph;
+
+/**
+ * Created by Archetype
+ */
+public interface IObjectGraph {
+    ObjectGraph getObjectGraph();
+}
