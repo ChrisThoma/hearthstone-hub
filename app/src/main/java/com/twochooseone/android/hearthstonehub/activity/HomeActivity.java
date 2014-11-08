4f7247071ef8235cf143bb936713057a711909a2
ChrisThoma <cthoma@vt.edu>
2014-11-08 02:08:50 -0500

    Added action bar hamburger.

--- app/src/main/java/com/twochooseone/android/hearthstonehub/activity/HomeActivity.java
+++ app/src/main/java/com/twochooseone/android/hearthstonehub/activity/HomeActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
 
 import com.twochooseone.android.hearthstonehub.R;
@@ -29,7 +30,28 @@ public class HomeActivity extends HearthstoneBaseActivity {
         ButterKnife.inject(this);
         setSupportActionBar(toolbar);
         getSupportActionBar().setTitle("HEARTHSTONE HUB");
+        drawerToggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.okay, R.string.oak_test) {
 
+        };
+
+        drawerLayout.setDrawerListener(drawerToggle);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeButtonEnabled(true);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (drawerToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        drawerToggle.syncState();
     }
 
 
